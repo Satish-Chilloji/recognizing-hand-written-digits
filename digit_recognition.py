449d82e5e841dc0a6446c2fe99c1f44a80e3b052
Satish-Chilloji <m22aie242@iitj.ac.in>
2023-09-07 10:23:55 +0530

    Assignment 3 is Implemented

--- digit_recognition.py
+++ digit_recognition.py
@@ -3,78 +3,36 @@
 
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
-
+import itertools
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, metrics, svm
-from sklearn.model_selection import train_test_split
+from utils import tune_hparams,train_dev_test_split
+from sklearn import svm,metrics
 
-def split_train_dev_test(X, y, test_size, dev_size):
-    # Split data into 50% train and 50% test subsets
-    X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=test_size, shuffle=False)
-    X_train, X_dev, y_train, y_dev = train_test_split(
-    X_train, y_train, test_size=dev_size, shuffle=False)
-    return X_train,X_test,X_dev,y_train,y_test,y_dev
-
-def predict_and_eval(model, X_test, y_test):
-    predicted=model.predict(X_test)
-
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, prediction in zip(axes, X_test, predicted):
-        ax.set_axis_off()
-        image = image.reshape(8, 8)
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title(f"Prediction: {prediction}")
-
-    print(
-    f"Classification report for classifier {model}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-    )
-
-    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-    disp.figure_.suptitle("Confusion Matrix")
-    print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-    plt.show()
-
-    # The ground truth and predicted lists
-    y_true = []
-    y_pred = []
-    cm = disp.confusion_matrix
-
-    # For each cell in the confusion matrix, add the corresponding ground truths
-    # and predictions to the lists
-    for gt in range(len(cm)):
-        for pred in range(len(cm)):
-            y_true += [gt] * cm[gt][pred]
-            y_pred += [pred] * cm[gt][pred]
-
-    print(
-        "Classification report rebuilt from confusion matrix:\n"
-        f"{metrics.classification_report(y_true, y_pred)}\n"
-    )
 
 if __name__=='__main__':
     digits = datasets.load_digits()
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, label in zip(axes, digits.images, digits.target):
-        ax.set_axis_off()
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title("Training: %i" % label)
 
     # flatten the images
     n_samples = len(digits.images)
     data = digits.images.reshape((n_samples, -1))
-
     X=data
     y=digits.target
 
-    X_train,X_test,X_dev,y_train,y_test,y_dev=split_train_dev_test(X,y,0.2,0.2)
+    gamma_ranges = [0.001, 0.01, 0.1, 1, 100]
+    C_ranges = [0.1, 1, 2, 5, 10]
 
-    # Create a classifier: a support vector classifier
-    model = svm.SVC(gamma=0.001)
+    list_of_all_param_combination = list(itertools.product(gamma_ranges, C_ranges))
 
-    # Learn the digits on the train subset
-    model.fit(X_train, y_train)
 
-    predict_and_eval(model,X_test,y_test)
+    test_size=[0.1,0.2,0.3]
+    dev_size=[0.1,0.2,0.3]
+    list_of_size=list(itertools.product(test_size, dev_size))
+    for test_frac, dev_frac in list_of_size:
+        X_train,Y_train, X_dev, Y_dev, X_test, Y_test=train_dev_test_split(X,y,test_frac,dev_frac)
+        model,gamma,C,cur_metric,train_metric=tune_hparams(X_train,Y_train,X_dev,Y_dev,list_of_all_param_combination)
+        predicted_test=model.predict(X_test)
+        test_metric = metrics.accuracy_score(y_pred=predicted_test, y_true=Y_test)
+        print(f'Train Size:{1-(test_frac+dev_frac)} Test Size:{test_frac} Dev Size:{dev_frac} Train Acc:{train_metric} Test Acc:{test_metric} Val Acc:{cur_metric}')
+        print(f'Found best metric for the SVM model with gamma:{gamma} and C:{C}')
+        print('\n')
---
+++ utils.py
@@ -0,0 +1,44 @@
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn import svm,metrics
+
+def preprocess_digits(dataset):
+    n_samples = len(dataset.images)
+    data = dataset.images.reshape((n_samples, -1))
+    label = dataset.target
+    return data, 
+
+def predict_and_eval(model, X_test, y_test):
+    predicted=model.predict(X_test)
+
+def train_dev_test_split(data, label, test_frac, dev_frac):
+
+    X_train_dev, X_test, Y_train_dev, Y_test = train_test_split(
+        data, label, test_size=test_frac, shuffle=True
+    )
+    X_train, X_dev, Y_train, Y_dev = train_test_split(
+        X_train_dev, Y_train_dev, test_size=dev_frac, shuffle=True
+    )
+
+    return X_train,Y_train, X_dev, Y_dev, X_test, Y_test
+
+def tune_hparams(X_train, Y_train, X_dev, Y_dev, list_of_all_param_combination):
+    best_acc_so_far=-1
+    best_train_metric=-1
+    for gamma, C in list_of_all_param_combination:
+        model=svm.SVC(C=C,gamma=gamma)
+        model.fit(X_train,Y_train)
+        predicted_dev=model.predict(X_dev)
+        cur_metric = metrics.accuracy_score(y_pred=predicted_dev, y_true=Y_dev)
+        predicted_train=model.predict(X_train)
+        train_metric = metrics.accuracy_score(y_pred=predicted_train, y_true=Y_train)
+
+        if cur_metric > best_acc_so_far:
+            best_acc_so_far = cur_metric
+            best_train_metric = train_metric
+            best_model=model
+            b_gamma=gamma
+            b_C=C
+    return best_model,b_gamma,b_C,best_acc_so_far,best_train_metric
+
+    
