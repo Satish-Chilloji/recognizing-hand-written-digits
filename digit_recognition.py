5695264cc5621a507a2d8432d297bb533914816f
Satish-Chilloji <m22aie242@iitj.ac.in>
2023-09-23 14:35:11 +0530

    Quiz qtns 3 is completed

--- digit_recognition.py
+++ digit_recognition.py
@@ -4,19 +4,21 @@
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
 import itertools
+import numpy as np
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, metrics, svm
 from utils import tune_hparams,train_dev_test_split
 from sklearn import svm,metrics
-
+from skimage.transform import resize
 
 if __name__=='__main__':
+    # Load the digits dataset from scikit-learn
     digits = datasets.load_digits()
 
     # flatten the images
     n_samples = len(digits.images)
     data = digits.images.reshape((n_samples, -1))
-    X=data
+    X=digits.images
     y=digits.target
 
     gamma_ranges = [0.001, 0.01, 0.1, 1, 100]
@@ -24,15 +26,33 @@ if __name__=='__main__':
 
     list_of_all_param_combination = list(itertools.product(gamma_ranges, C_ranges))
 
-
-    test_size=[0.1,0.2,0.3]
-    dev_size=[0.1,0.2,0.3]
+    test_size=[0.2]
+    dev_size=[0.1]
+    # Define a list of image sizes to evaluate
+    image_sizes = [(4, 4), (6, 6), (8, 8)]
     list_of_size=list(itertools.product(test_size, dev_size))
-    for test_frac, dev_frac in list_of_size:
-        X_train,Y_train, X_dev, Y_dev, X_test, Y_test=train_dev_test_split(X,y,test_frac,dev_frac)
-        model,gamma,C,cur_metric,train_metric=tune_hparams(X_train,Y_train,X_dev,Y_dev,list_of_all_param_combination)
-        predicted_test=model.predict(X_test)
-        test_metric = metrics.accuracy_score(y_pred=predicted_test, y_true=Y_test)
-        print(f'Train Size:{1-(test_frac+dev_frac)} Test Size:{test_frac} Dev Size:{dev_frac} Train Acc:{train_metric} Test Acc:{test_metric} Val Acc:{cur_metric}')
-        print(f'Found best metric for the SVM model with gamma:{gamma} and C:{C}')
-        print('\n')
+
+
+# Define a function to resize images and evaluate performance
+def image_resize(image_size,X_train,X_dev,X_test):
+    # Resize the training and testing images
+    X_train_resized = np.array([resize(image, image_size, anti_aliasing=True) for image in X_train])
+    X_test_resized = np.array([resize(image, image_size, anti_aliasing=True) for image in X_test])
+    X_dev_resized = np.array([resize(image, image_size, anti_aliasing=True) for image in X_dev])
+    # Flatten the resized images
+    X_train_flat = X_train_resized.reshape(len(X_train_resized), -1)
+    X_test_flat = X_test_resized.reshape(len(X_test_resized), -1)
+    X_dev_flat = X_dev_resized.reshape(len(X_test_resized), -1)
+    return X_train_resized,X_test_resized,X_dev_resized
+
+for size in list_of_size:
+    X_train,Y_train, X_dev, Y_dev, X_test, Y_test=train_dev_test_split(X,y,0.2,0.1)
+    print(X_train.shape)
+    X_train_flat,X_dev_flat,X_test_flat=image_resize(size,X_train,X_dev,X_test)
+    print(X_train_flat.shape)
+    model,gamma,C,cur_metric,train_metric=tune_hparams(X_train_flat,Y_train,X_dev_flat,Y_dev,list_of_all_param_combination)
+    predicted_test=model.predict(X_test_flat)
+    test_metric = metrics.accuracy_score(y_pred=predicted_test, y_true=Y_test)
+    print(f'Image Size:{size} Train Size:{1-(0.2+0.1)} Test Size:{0.2} Dev Size:{0.1} Train Acc:{train_metric} Test Acc:{test_metric} Val Acc:{cur_metric}')
+    print(f'Found best metric for the SVM model with gamma:{gamma} and C:{C}')
+    print('\n')
---
+++ test.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from skimage.transform import resize
+from utils import tune_hparams,train_dev_test_split
+
+# Load the digits dataset from scikit-learn
+digits = datasets.load_digits()
+
+# Split the dataset into training and testing sets
+X_train,y_train, X_dev, Y_dev, X_test, y_test = train_dev_test_split(digits.images, digits.target, 0.2,0.1)
+
+# Define a function to resize images and evaluate performance
+def evaluate_performance(image_size):
+    # Resize the training and testing images
+    X_train_resized = np.array([resize(image, image_size, anti_aliasing=True) for image in X_train])
+    X_test_resized = np.array([resize(image, image_size, anti_aliasing=True) for image in X_test])
+    
+    # Flatten the resized images
+    X_train_flat = X_train_resized.reshape(len(X_train_resized), -1)
+    X_test_flat = X_test_resized.reshape(len(X_test_resized), -1)
+    
+    # Train a Support Vector Machine (SVM) classifier
+    svm_classifier = SVC(kernel='linear', C=1)
+    svm_classifier.fit(X_train_flat, y_train)
+    
+    # Predict on the test set
+    y_pred = svm_classifier.predict(X_test_flat)
+    
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    return accuracy
+
+# Define a list of image sizes to evaluate
+image_sizes = [(4, 4), (6, 6), (8, 8)]
+
+# Evaluate performance for each image size
+for size in image_sizes:
+    accuracy = evaluate_performance(size)
+    print(f"Image Size {size}: Accuracy = {accuracy:.2f}")
