296f78f272d18e71fd996558013dd575a7593100
Satish-Chilloji <m22aie242@iitj.ac.in>
2023-10-27 09:35:44 +0530

    added the pytest test cases

--- test_utils.py
+++ test_utils.py
@@ -13,23 +13,21 @@ def test_wrong_answer():
 # def test_wrong_answer():
 #     assert inc(3) == 5
 
-
 def test_hyparam_combinations():
-    gamma_list=[0.001,0.01]
-    C_list=[1]
-    # gamma_list=[0.001,0.01,0.1,1]
-    # C_list=[1,10,100,1000]
+    # Test case to check that all the possible combination of paramas are indeed generated.
+    gamma_list=[0.001,0.01,0.1,1]
+    C_list=[1,10,100,1000]
     h_params={}
     h_params['gamma']=gamma_list
     h_params['C']=C_list
     h_params_combinations=get_hyperparameter_combinations(h_params)
-    # assert len(h_params_combinations)==len(gamma_list)*len(C_list)
+    assert len(h_params_combinations)==len(gamma_list)*len(C_list)
     expected_param_combo_1={'gamma':0.001,'C':1}
     expected_param_combo_2={'gamma':0.01,'C':1}
     assert (expected_param_combo_1 in h_params_combinations) and (expected_param_combo_2 in h_params_combinations)
 
-def test_data_splitting():
-    X,y=read_digits()
+# def test_data_splitting():
+#     X,y=read_digits()
 
-    X=X[:100,:]
-    y=
+#     X=X[:100,:]
+#     y=
