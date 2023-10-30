b236c58eedc092a9678c862bcafd6f126cd444f1
Satish-Chilloji <m22aie242@iitj.ac.in>
2023-10-30 22:39:21 +0530

    Chaging the docker image to run the model

--- test_utils.py
+++ test_utils.py
@@ -1,37 +1,64 @@
-from utils import get_hyperparameter_combinations
-from utils import train_dev_test_split,read_digits
-
-def inc(x):
-    return x + 1
-
-def test_answer():
-    assert inc(3) == 4
-
-def test_wrong_answer():
-    assert not inc(3) == 5
-
-def test_hyparam_combinations():
-    # Test case to check that all the possible combination of paramas are indeed generated.
-    gamma_list=[0.001,0.01,0.1,1]
-    C_list=[1,10,100,1000]
+from utils import get_hyperparameter_combinations, train_test_dev_split,read_digits, tune_hparams, preprocess_data
+import os
+def test_for_hparam_cominations_count():
+    # a test case to check that all possible combinations of paramers are indeed generated
+    gamma_list = [0.001, 0.01, 0.1, 1]
+    C_list = [1, 10, 100, 1000]
     h_params={}
-    h_params['gamma']=gamma_list
-    h_params['C']=C_list
-    h_params_combinations=get_hyperparameter_combinations(h_params)
-    assert len(h_params_combinations)==len(gamma_list)*len(C_list)
-    expected_param_combo_1={'gamma':0.001,'C':1}
-    expected_param_combo_2={'gamma':0.01,'C':1}
+    h_params['gamma'] = gamma_list
+    h_params['C'] = C_list
+    h_params_combinations = get_hyperparameter_combinations(h_params)
+    
+    assert len(h_params_combinations) == len(gamma_list) * len(C_list)
+
+def create_dummy_hyperparameter():
+    gamma_list = [0.001, 0.01]
+    C_list = [1]
+    h_params={}
+    h_params['gamma'] = gamma_list
+    h_params['C'] = C_list
+    h_params_combinations = get_hyperparameter_combinations(h_params)
+    return h_params_combinations
+def create_dummy_data():
+    X, y = read_digits()
+    
+    X_train = X[:100,:,:]
+    y_train = y[:100]
+    X_dev = X[:50,:,:]
+    y_dev = y[:50]
+
+    X_train = preprocess_data(X_train)
+    X_dev = preprocess_data(X_dev)
+
+    return X_train, y_train, X_dev, y_dev
+def test_for_hparam_cominations_values():    
+    h_params_combinations = create_dummy_hyperparameter()
+    
+    expected_param_combo_1 = {'gamma': 0.001, 'C': 1}
+    expected_param_combo_2 = {'gamma': 0.01, 'C': 1}
+
     assert (expected_param_combo_1 in h_params_combinations) and (expected_param_combo_2 in h_params_combinations)
 
-def test_data_split():
-    X, y =read_digits()
-    #import pdb; pdb.set_trace()
-    X = X[:100,:]
-    y = y[:100]
-    test_size = 0.1
-    dev_size = 0.6
-    train_size = 1 - test_size - dev_size
+def test_model_saving():
+    X_train, y_train, X_dev, y_dev = create_dummy_data()
+    h_params_combinations = create_dummy_hyperparameter()
 
-    X_train,Y_train, X_dev, Y_dev, X_test, Y_test = train_dev_test_split(X, y, test_frac=test_size, dev_frac=dev_size)
-    print(len(X))
-    assert (len(X_train) == int(len(X)*train_size)) and (len(X_test) == len(X)*test_size) and (len(X_dev) == len(X)*dev_size)
+    _, best_model_path, _ = tune_hparams(X_train, y_train, X_dev, 
+        y_dev, h_params_combinations)   
+
+    assert os.path.exists(best_model_path)
+
+def test_data_splitting():
+    X, y = read_digits()
+    
+    X = X[:100,:,:]
+    y = y[:100]
+    
+    test_size = .1
+    dev_size = .6
+
+    X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
+
+    assert (len(X_train) == 30) 
+    assert (len(X_test) == 10)
+    assert  ((len(X_dev) == 60))
