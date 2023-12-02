40479ff97ee0e46d40c162e44e3549bdf7e5a995
Satish-Chilloji <m22aie242@iitj.ac.in>
2023-12-02 19:46:25 +0530

    Q5. Docker and Cloud Container Registry

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,5 @@
 docker build -t base -f docker/DependencyDockerfile .
 docker build -t digits -f Dockerfile .
 docker run -it digits
-az acr build --file docker/Dockerfile --registry satimlops23 --image base .
-az acr build --file Dockerfile --registry satimlops23 --image digits .
-
-# docker tag digits:latest satimlops23.azurecr.io/digits:latest
-# docker push satimlops23.azurecr.io/digits:latest
+docker tag digits:latest satimlops23.azurecr.io/digits:latest
+docker push satimlops23.azurecr.io/digits:latest
